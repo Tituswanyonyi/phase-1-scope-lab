6103cca3f0a1f8f32409a0e0ca4f600d72c42077
titus <titus.unavailable@student.moringaschool.com>
2023-03-28 20:25:44 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 // Write your solution in this file!
-var customerName ='bob';
-function upperCaseCustomerName(){
-  customerName= customerName.toUpperCase();
+function scuberGreetingForFeet(distance){
+  // Write your code here!
+  if(distance <=400 ){
+    return 'This one is on me!';
+  }
+  else if(distance <2500){
+  return 'I will gladly take your thirty bucks.';
+  }
+  else if(distance >2500){
+    return 'No can do.' ;
+
 }
 
-function setBestCustomer(){
-   bestCustomer = 'not bob';
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+return city =='NYC'?'Ok, sounds good.':'No go.';
 }
 
-function overwriteBestCustomer(){
-  bestCustomer = 'maybe bob';
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+  default:
+  return 'Bye.';
+    break;
 }
 
-const leastFavoriteCustomer ='some initial value';
-
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'new name';
 } 
